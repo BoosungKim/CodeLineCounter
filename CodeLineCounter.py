c50028ff6ea59c2e0d243da553b317d7066b32fc
Bu Sung Kim <superbsung@gmail.com>
2015-12-22 20:38:42 +0900

    Finish

--- CodeLineCounter.py
+++ CodeLineCounter.py
@@ -1,24 +1,23 @@
 import os
 import time
 import locale
+import sys
 
-gonna_counted_extensions = ["cpp", "h", "hpp", "c", "rc", "rc2"]
+gonna_counted_extensions = ["cpp", "h", "hpp"]
 
 
 def count_lines_in_file(path, filename):
     valid_code_line = 0
 
     file_url = path + '\\' + filename
-    input_file = open(file_url, mode="r")
+    input_file = open(file_url, mode="rt", encoding="utf-8", errors="ignore")
 
     try:
         for line in input_file.readlines():
             if len(line) > 1:
                 valid_code_line += 1
     except UnicodeDecodeError as e:
-        print(e)
-
-
+        print(filename, e)
 
     return valid_code_line
 
@@ -29,14 +28,11 @@ def scan_dir_for_count_loc(path):
     dirs = [x for x in os.scandir(path) if x.is_dir()]
     files = [x for x in os.scandir(path) if not x.is_dir()]
 
-    # extensions_in_files = set(map(lambda x: x.name.split('.')[-1], files))
-
     # Calculate # of file lines
     for each_file in files:
         extension = each_file.name.split('.')[-1]
         if extension in gonna_counted_extensions:
             ret_code_line += count_lines_in_file(path, each_file.name)
-            # print(filename + " : %d" % count_lines_in_file(path, filename))
 
     # Recursively calculate each directory
     for each_dir in dirs:
@@ -47,13 +43,18 @@ def scan_dir_for_count_loc(path):
 
 
 if __name__ == '__main__':
-    start_time = time.time()
+    if len(sys.argv) < 2:
+        print("Usage => \CodeLineCounter.py path1 path2 ...")
+    else:
+        total_loc_int = 0
+        start_time = time.time()
 
-    sample_path = r"D:\MIDAS\CAED trunk"
-    # sample_filename = r"CAE_mesh.cpp"
-    # print(count_lines_in_file(sample_path, sample_filename))
-    total_loc = scan_dir_for_count_loc(sample_path)
+        for i in range(1, len(sys.argv)):
+            path_name_str = sys.argv[i]
+            loc_int = scan_dir_for_count_loc(path_name_str)
+            print(path_name_str, " : ", loc_int)
+            total_loc_int += loc_int
 
-    print(locale.format("%d", total_loc, grouping=True))
-    print("Elapsed time : %.2f" % (time.time() - start_time))
-
+        locale.setlocale(locale.LC_ALL, "")
+        print("Total LOC : " + locale.format("%d", total_loc_int, grouping=True))
+        print("Elapsed time : %.2f" % (time.time() - start_time))
